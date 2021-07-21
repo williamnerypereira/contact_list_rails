c4b5fc3a95509933f098922adb30bab70fca9c41
williamnerypereira <william.nery.pereira@gmail.com>
2021-07-21 18:32:37 -0300

    Projeto Final

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+
+  def new
+    redirect_to user_path(current_user) if user_signed_in?
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      sign_in(user)
+      redirect_to contacts_path
+    else
+      flash.now[:danger] = 'E-mail ou senha inválidos'
+      render 'new'
+    end
+  end
+
+  def destroy
+    sign_out 
+    flash[:success] = 'Logout com sucesso'
+    redirect_to entrar_path
+  end
+
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+    before_action :require_logged_in_user, only: [:edit, :update]
+
+    def new
+        @user = User.new             
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = 'Usuário cadastrado com sucesso'
+            redirect_to root_url
+        else
+            render 'new'
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        if current_user.update(user_params)
+            flash[:success] = 'Dados atualizados com sucesso'
+            redirect_to contacts_url
+        else
+            render 'edit'
+        end
+    end
+
+    private 
+        def user_params
+            params.require(:user).permit(:email, :name, :password, :password_confirmation)
+        end
+
+end
